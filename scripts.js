4444cd8d7d30e7ad0441e5375e041425bb2427a3
m3ia <meianatividad@gmail.com>
2022-08-10 19:18:19 -0700

    Get contact form to log data in console

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@ const aboutDiv = doc.getElementsByClassName('aboutMe')[0];
 // About Me link:
 const aboutMeLink = doc.getElementById('aboutMeLink');
 // Contact Form section:
-const contactForm = doc.getElementsByClassName('contactFormDiv')[0];
+const contactFormDiv = doc.getElementsByClassName('contactFormDiv')[0];
 // Contact Form link
 const contactLink = doc.getElementById('contactLink');
 
@@ -21,9 +21,9 @@ const contactLink = doc.getElementById('contactLink');
 
 // About Me link:
 aboutMeLink.addEventListener("click", function() {
-    contactForm.classList.remove("w3-animate-opacity", "w3-animate-bottom");
+    contactFormDiv.classList.remove("w3-animate-opacity", "w3-animate-bottom");
     aboutDiv.classList.add("w3-animate-opacity", "w3-animate-bottom");
-    contactForm.style.display = 'none';
+    contactFormDiv.style.display = 'none';
     aboutDiv.style.display = 'block';
 })
 
@@ -31,7 +31,45 @@ aboutMeLink.addEventListener("click", function() {
 // Contact Form link:
 contactLink.addEventListener("click", function() {
     aboutDiv.classList.remove("w3-animate-opacity", "w3-animate-bottom");
-    contactForm.classList.add("w3-animate-opacity", "w3-animate-bottom");
+    contactFormDiv.classList.add("w3-animate-opacity", "w3-animate-bottom");
     aboutDiv.style.display = 'none';
-    contactForm.style.display = 'block';
+    contactFormDiv.style.display = 'block';
+})
+
+// ------------------------------------------------------------------------------------
+// CONTACT FORM SUBMISSION
+// ------------------------------------------------------------------------------------
+const submissionName = doc.getElementById('inputName');
+const submissionEmail = doc.getElementById('inputEmail');
+const submissionMsg = doc.getElementById('inputMessage');
+
+submissionName.addEventListener("input", e => {
+    const name = e.target.value;
+    return name;
+})
+
+submissionEmail.addEventListener("input", e => {
+    const email = e.target.value;
+    return email;
+})
+
+submissionMsg.addEventListener("input", e => {
+    const msg = e.target.value;
+    return msg;
+})
+
+const contactForm = doc.querySelector('form.contactForm');
+
+contactForm.addEventListener("submit", e => {
+    e.preventDefault();
+    console.log('name: ', submissionName.value);
+    console.log('email: ', submissionEmail.value);
+    console.log('message: ', submissionMsg.value);
+
+    const subName = doc.getElementById('inputName');
+    const subEmail = doc.getElementById('inputEmail');
+    const subMsg = doc.getElementById('inputMessage');
+    subName.value = "";
+    subEmail.value = "";
+    subMsg.value = "";
 })
