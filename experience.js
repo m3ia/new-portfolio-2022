4b1f448216a12c00d8a50be7bd2db9a74d0cb794
m3ia <meianatividad@gmail.com>
2022-08-21 10:08:35 -0700

    Fix up some experience and project descriptions

--- experience.js
+++ experience.js
@@ -4,20 +4,20 @@ export const experiences = [
     "Company Link": "https://techtonica.org/",
     "Role": "Software Engineer Apprentice",
     "Date Range": "JUL 2022 - PRESENT",
-    "Experience": ["Conceptualized, developed, and designed capstone project for a web application: Got It!", "Built and deployed full-stack web applications using PostgreSQL, Express, React, Node, JavaScript, HTML, & CSS", "Collaborated and peer-mentored daily over code challenges", "Led weekly team meetings and peer code reviews"]
+    "Experience": ["Conceptualized, developed, and designed capstone project for a web application: Got It!", "Built and deployed full-stack web applications using PostgreSQL, Express, React, Node, JavaScript, HTML, & CSS", "Led weekly team meetings and collaborated on peer code reviews"]
   },
   {
     "Company": "Stack Overflow",
     "Company Link": "https://stackoverflow.com/",
     "Role": "Contract Developer",
     "Date Range": "JUL 2021 - DEC 2021",
-    "Experience": ["Completed tickets adding features, conducting tech discoveries, and fixing bugs", "Shadowed and collaborated with engineers on 2 teams", "Contributed to sunsetting a major user-facing service through investigating the impact of deprecating code and testing user flows"],
+    "Experience": ["Completed tickets adding features, conducting tech discoveries, and fixing bugs using C#, ASP.NET, Entity Framework, and Stacks for CSS", "Contributed to sunsetting a major user-facing service by investigating the impact of deprecating code and testing user flows", "Supported higher-level engineers building a brand new ad service",],
   },
   {
     "Company": "Yelp",
     "Company Link": "https://yelp.com/",
     "Role": "Senior Content Moderator",
     "Date Range": "JAN 2016 - JAN 2021",
-    "Experience": ["Directed and supported the team that moderates media-fueled site abuse, recovering ~320 impacted business listings per month", "Q3 2020: Received the Above & Beyond award for championing the MAA team during a challenging quarter", "Worked cross-functionally with Legal, Support, and Fraud Analytics teams to provide second evaluations for 120+ contested weekly cases while maintaining a 1-2 day turnaround time"]
+    "Experience": ["Directed and supported team that moderated media-fueled site abuse with an average of ~320 businesses protected per month", "Worked cross-functionally with Legal, Fraud Analytics, and User Support to lead the Case Escalations team, closing 500+ contested weekly cases while maintaining a 1-2 day turnaround time", "Q3 2020: Received the Above & Beyond award for championing 2 key teams during a challenging quarter"]
   },
 ];
--- module.js
+++ module.js
@@ -2,7 +2,7 @@ export let projects = [
 {
   'Name': `Sudoku Puzzle`,
   'Build': `JS, HTML, CSS`,
-  'Description': `For Techtonica, we had 1 week to build a game of our choice with just HTML, CSS, and JavaScript, so I chose a game I'm passionate about: a Sudoku puzzle.`,
+  'Description': `For Techtonica, I spent ~4 days building a game I'm passionate about: Sudoku. My favorite part building this game was figuring out an algorithm to dynamically update the styling according to duplicates in a row, column, and a parent box grid.`,
   'Image': `./images/sudoku.png`,
   'Link': `https://github.com/m3ia/sudoku_v2`,
   'Project Type': `Passion Project`,
@@ -10,7 +10,7 @@ export let projects = [
   {
   'Name': `Mock Recipe Blog Page`,
   'Build': `JS, HTML, CSS`,
-  'Description': `This is a mock recipe site with a customized logo, navbar, and a recipe section. This project pushed me to better organize my code and deepen my familiarity with CSS.`,
+  'Description': `For this project, I practiced designing a complete recipe page. I really enjoyed working on this project to deepen my knowledge of CSS. (Also because I featured a recipe for one of my favorite snacks.)`,
   'Image': `./images/mock-recipe.png`,
   'Link': `https://github.com/m3ia/mock-recipe-page/blob/main/README.md`,
   'Project Type': `Passion Project`,
@@ -18,7 +18,7 @@ export let projects = [
 {
   'Name': `Got It!`,
   'Build': `Node.js, React.js, Express.js, Postgres, Google Cal API, Google, OAuth2, Bootstrap`,
-  'Description': `Got It! is a shopping list app for anyone who loves to cook, prep, and plan on the regular. \n Got It has a recurring list feature where you can stay on top of your recurring shopping lists on a regular basis.`,
+  'Description': `Got It! is a shopping list app for anyone who loves to cook, prep, and plan on a regular basis. \n Got It has a recurring list feature where you can stay on top of your recurring shopping lists on a regular basis.`,
   'Image': `./images/got-it.png`,
   'Link': `https://github.com/m3ia/GotIt`,
   'Project Type': `Passion Project`,
@@ -43,7 +43,7 @@ export let projects = [
 {
   'Name': `Listings Practice`,
   'Build': `React, Tailwind CSS`,
-  'Description': `I used this project to practice dynamically updating a view of mock-up property listings according to 3 ways of searching: \n 1. Typing a property name in the search bar \n 2. Selecting/deselecting amenities \n 3. Filtering by occupancy range.`,
+  'Description': `I used this project to practice dynamically updating a view of mock-up property listings according to 3 ways of searching: \n typing a property name in the search bar, \n selecting/deselecting amenities, \n and filtering by occupancy range.`,
   'Image': `./images/listings-practice.png`,
   'Link': `https://github.com/m3ia/listings-practice`,
   'Project Type': `#PracticeMakesProgress`,
